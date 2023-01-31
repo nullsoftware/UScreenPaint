9028c90addec45a2f4d61ad41cd193934514cb10
nullsoftware <tumyrthebot@gmail.com>
2023-02-01 01:06:46 +0200

    Added support for Russian and Ukrainian languages.

--- UScreenPaint/App.xaml.cs
+++ UScreenPaint/App.xaml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,6 +17,45 @@ namespace UScreenPaint
     /// </summary>
     public partial class App : Application
     {
-        public static Version Version { get; } = Assembly.GetEntryAssembly().GetName().Version;
+        public static Assembly CurrentAssembly { get; } = Assembly.GetEntryAssembly();
+
+        public static Version Version { get; } = CurrentAssembly.GetName().Version;
+
+        internal string[] SupportedLanguages { get; } = new string[] 
+            {
+                "ru",
+                "uk",
+            };
+
+        public App()
+        {
+            
+        }
+
+        private void OnApplicationStartup(object sender, StartupEventArgs e)
+        {
+            SetupLanguage();
+        }
+
+        private void SetupLanguage()
+        {
+            string currentLanguageCode = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+
+            if (SupportedLanguages.Contains(currentLanguageCode))
+            {
+                ResourceDictionary langDir = Resources.MergedDictionaries.First(t => t.Source.ToString().EndsWith("/Resources.xaml"));
+                Resources.MergedDictionaries.Remove(langDir);
+                Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri($"Assets/Localization/Resources.{currentLanguageCode}.xaml", UriKind.Relative)
+                });
+            }
+            //else
+            //{
+            //    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            //    CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+            //    Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            //}
+        }
     }
 }
